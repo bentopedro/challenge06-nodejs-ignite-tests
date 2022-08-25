1c605b08c2c9e0492e0856936f27fa1cb8d91f14
Bento Pedro <bentomenni.kidima@gmail.com>
2022-08-25 15:30:35 +0100

    Test Create Users

---
+++ src/modules/users/useCases/createUser/CreateUserUseCase.spec.ts
@@ -0,0 +1,30 @@
+import { STATUS_CODES } from "http";
+import { InMemoryUsersRepository } from "../../repositories/in-memory/InMemoryUsersRepository";
+import { CreateUserUseCase } from "./CreateUserUseCase";
+import { ICreateUserDTO } from "./ICreateUserDTO"
+
+
+let userRepositoryInMemory: InMemoryUsersRepository;
+let createUserUseCase: CreateUserUseCase;
+
+describe("Create User", () => {
+
+  beforeEach(() => {
+    userRepositoryInMemory = new InMemoryUsersRepository()
+    createUserUseCase = new CreateUserUseCase(userRepositoryInMemory)
+  })
+
+  it("should be able to create a new user", async () => {
+    const user: ICreateUserDTO = {
+      name: "New User",
+      email: "test@emai.com",
+      password: "supersenha123",
+    }
+
+    const resp = await createUserUseCase.execute(user)
+
+    console.log(resp);
+
+    // expect(STATUS_CODES).toBe(201)
+  })
+})
